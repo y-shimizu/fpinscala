d1f96e2f09f2ba2efe1a6faab005160d6f1b0860
Yoshihiro Shimizu <shimizu@amoad.com>
2014-07-04 17:36:50 +0900

    chepeter2_fin

--- src/main/scala/chapter2/Recursive.scala
+++ src/main/scala/chapter2/Recursive.scala
@@ -1,13 +1,33 @@
 package chapter2
 
-object Recursive extends App {
+object Recursive {
   //Exercise 1
   def fib(n: Int): Int = {
+    @annotation.tailrec
     def go(n1: Int, n2: Int, count: Int): Int = {
       if (count == 0) n1
-      else if (count == 1) n2
       else go(n2, n1 + n2, count -1)
     }
     go(0, 1, n)
   }
+
+  //Exercise 2
+  def isSorted[A](as: Array[A], gt: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def go(n: Int): Boolean = {
+      if (n < 1) true
+      else gt(as(n - 1), as(n)) && go(n - 1)
+    }
+    go(as.length - 1)
+  }
+
+  //Exercise 3
+  def curry[A,B,C](f: (A, B) => C): A => (B => C) = (a: A) => (b: B) => f(a,b)
+
+  //Exercise 4
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = (a: A, b: B) => f(a)(b)
+
+  //Exercise 5
+  def compose[A,B,C](f: B => C, g: A => B): A => C = (a: A) => f(g(a))
+
 }
--- src/test/scala/chapter2/RecursiveSpec.scala
+++ src/test/scala/chapter2/RecursiveSpec.scala
@@ -1,15 +1,23 @@
 package chapter2
 
-import org.specs2.mutable.Specification
+import org.specs2.mutable.SpecificationWithJUnit
 
 
-class RecursiveSpec extends Specification {
+class RecursiveSpec extends SpecificationWithJUnit {
   //Exercise 1
   "fib" should {
     "return fibonacci sequence" in {
       Recursive.fib(0) must_== 0
       Recursive.fib(1) must_== 1
       Recursive.fib(7) must_== 13
+    }
+  }
+
+  //Exercise 2
+  "isSorted" should {
+    "return true or false" in {
+      Recursive.isSorted(Array(1,4,5,6,9), (a: Int, b: Int) => a < b) must_== true
+      Recursive.isSorted(Array(1,4,2,6,3), (a: Int, b: Int) => a < b) must_== false
     }
   }
 
